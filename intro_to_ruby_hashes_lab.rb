261466be8f5f48d6f82f33f241a10201f947cea3
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-28 18:54:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,7 +38,7 @@ def monopoly_with_third_tier
 	   pieces: 4,
 	   names:
 	   {
-	     reading_railroad: {}
+	     reading_railroad: {},
 	     pennsylvania_railroad: {},
 	     b_and_o_railroad: {},
 	     shortline: {}
@@ -63,10 +63,22 @@ def monopoly_with_fourth_tier
 	   pieces: 4,
 	   names:
 	   {
-	     reading_railroad: {}
-	     pennsylvania_railroad: {},
-	     b_and_o_railroad: {},
-	     shortline: {}
+	     reading_railroad: 
+	     {
+	       mortgage_value: "$100"
+	     },
+	     pennsylvania_railroad: 
+	     {
+	       mortgage_value: "$200"
+	     },
+	     b_and_o_railroad: 
+	     {
+	       mortgage_value: "$400"
+	     },
+	     shortline: 
+	     {
+	       mortgage_value: "$800"
+	     }
 	   },
 	   rent_in_dollars:
 	   {
